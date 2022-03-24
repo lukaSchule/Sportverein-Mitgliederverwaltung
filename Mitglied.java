01de80bd4cc12ca85039bf78c458a6358ad9ff7b
Luka <luka.elkmann@mga.bistum365.de>
2022-03-24 20:09:23 +0100

    d

---
+++ Mitglied.java
@@ -0,0 +1,14 @@
+/**
+ * Abstrakte Klasse Mitglied - beschreiben Sie hier die Klasse
+ * 
+ * @author (Ihr Name)
+ * @version (eine Version-Nummer oder ein Datum)
+ */
+public abstract class Mitglied
+{
+    String nachName;
+    String vorname;
+    String anmeldedatum;
+    
+    
+}
