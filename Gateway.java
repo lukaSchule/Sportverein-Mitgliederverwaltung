f3511457e7832937d7611ab8742d78a7ec7791f4
Luka <luka.elkmann@mga.bistum365.de>
2022-03-31 20:42:31 +0200

    D

--- Gateway.java
+++ Gateway.java
@@ -16,16 +16,60 @@ public class Gateway
     }
 
     /**
-     * Diese Methode setzt die CREATE-Funktion um, indem hier neue Highscores in die Datenbank eingetragen werden.
+     * Diese Methode setzt die CREATE-Funktion um, indem hier ein neues Mitglied in die Datenbank Mitglieder eingetragen wird.
+     * Der Status wird dabei automatisch ermittelt, jenachdem was für ein Typ übergeben wird.
      * 
-     * @param name
-     * @param punkte
+     * @param Nachname 
+     * @param Vorname
+     * @param Adresse
+     * @param Typ Dies ist der Typ des Mitglieds, also Spieler, Trainer, Fan, Sponsor oder Koordinator
      */
-    public void hinzufuegen(String name, int punkte)
+    public void hinzufuegen(String Nachname, String Vorname, String Adresse, String Anmeldedatum, String Typ, String Position, int Aufwandsentschaedigung, int Sponsoring)
     {
+        String status;
+        if(Typ.equalsIgnoreCase("Spieler") || Typ.equalsIgnoreCase("Trainer") || Typ.equalsIgnoreCase("Koordinater")){
+            status = "Aktiv";    
+        }else{
+            status = "Passiv";    
+        }
+        
+        if(!Typ.equalsIgnoreCase("Sponsor")){
+            Sponsoring = 0;    
+        }
+        
+        if(status == "Passiv"){
+            Aufwandsentschaedigung = 0;    
+        }
+        
         verbinde();
-        db.executeStatement("INSERT INTO highscore (name, punkte) VALUES ('"+name+"', "+punkte+")");
+        db.executeStatement("INSERT INTO Mitglieder (Nachname, Vorname, Adresse, Anmeldedatum, Typ, Status, Aufwandsentschaedugung, Sponsoring) VALUES ('"+Nachname+"', '"+Vorname+"', '"+Adresse+"', '"+Anmeldedatum+"', '"+Typ+"', '"+status+"', "+Aufwandsentschaedigung+", "+Sponsoring+")");
         beende();
+    }
+    
+    public void executeStatement(String statement){
+        db.executeStatement(statement);    
+    }
+    
+    public int gibSummeSponsoring(){
+        db.executeStatement("SELECT SUM(Sponsoring) FROM Mitglieder");    
+        QueryResult ergebnis = db.getCurrentQueryResult();
+        int erg = Integer.parseInt(ergebnis.getData()[0][0]);
+        return erg;
+    }
+    
+    public int gibAuflaufpraemien(){
+        db.executeStatement("SELECT SUM(Einsaetze) FROM Spieler");       
+        QueryResult ergebnis = db.getCurrentQueryResult();
+        int einsaetze = Integer.parseInt(ergebnis.getData()[0][0]);
+        return einsaetze * 50;
+    }
+    
+    public int gibSummeAusgaben(){
+        db.executeStatement("SELECT SUM(Aufwandsentschaedigung) FROM Mitglieder");   
+        QueryResult ergebnis = db.getCurrentQueryResult();
+        int aufwandsentschaedigung = Integer.parseInt(ergebnis.getData()[0][0]);
+        int auflaufpraemien = gibAuflaufpraemien();
+        return aufwandsentschaedigung + auflaufpraemien;
     }
     
     /**
@@ -36,7 +80,12 @@ public class Gateway
     public void loesche(int id)
     {
         verbinde();
-        db.executeStatement("DELETE FROM highscore WHERE id ="+id);
+        db.executeStatement("DELETE FROM Mitglieder WHERE id ="+id);
+        try{
+            db.executeStatement("DELETE FROM Spieler WHERE id ="+id);   
+        }catch(Exception e){
+            
+        }
         beende();
     }
     
@@ -55,12 +104,13 @@ public class Gateway
     }
     
     /**
-     * Diese Methode erzeugt die Tabelle highscore, wenn diese nicht schon exisitiert.
+     * Diese Methode erzeugt die Tabelle Mitglieder, wenn diese nicht schon exisitiert.
      */
     public void erzeugeTabelle()
     {
          verbinde();
-         db.executeStatement("Create table if not exists highscore (id SERIAL PRIMARY_KEY, name Varchar(255), punkte int)");
+         db.executeStatement("Create table if not exists Mitglieder(VereinsID SERIAL PRIMARY_KEY, Nachname VARCHAR(40), Vorname VARCHAR(40), Adresse VARCHAR(100), Anmeldedatum Varchar(25), Typ VARCHAR(20), Status BOOLEAN, Aufwandsentschaedigung int, Sponsoring int)");
+         db.executeStatement("Create table if not exists Spieler(VereinsID int PRIMARY_KEY REFERENCES Mitglieder(VereinsID), Einsaetze INT, Tore INT, Position Varchar(255)");
          beende();
     }
     
@@ -71,7 +121,7 @@ public class Gateway
     {
         if(db == null)
         {
-            db = new DatabaseConnector("",0,"spielstand","","");
+            db = new DatabaseConnector("",0,"Mitglieder","","");
         }
     }
     
--- Verwaltung.java
+++ Verwaltung.java
@@ -7,7 +7,27 @@
  */
 public class Verwaltung
 {
+    Gateway gate;
     public Verwaltung(){
-            
+        gate = new Gateway();        
+    }
+    
+    public void fuegeMitgliedHinzu(String Nachname, String Vorname, String Adresse, String Anmeldedatum, String Typ, String Position, int Aufwandsentschaedigung, int Sponsoring){
+        gate.hinzufuegen(Nachname, Vorname, Adresse, Anmeldedatum, Typ, Position, Aufwandsentschaedigung, Sponsoring); 
+            }
+    public void loescheMitglied(int id){
+        gate.loesche(id);       
+    }
+    public void gibSponsoring(){
+        gate.gibSummeSponsoring();        
+    }
+    public boolean vergleicheSponsorgeldMitAusgaben(){
+        int sponsoring = gate.gibSummeSponsoring();
+        int ausgaben = gate.gibSummeAusgaben();
+        if(ausgaben > sponsoring){
+            return true;       
+        }
+        else
+            return false;
     }
 }
